a102aef200719e206fcba2c94c9224e775f234a6
masfuulaji <masfuulaji@gmail.com>
2022-04-05 22:33:52 +0700

    add slider component

---
+++ src/components/partial/Slider.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="slider-wrapper">
+        <Splide :options="sliderOptions" aria-label="My Favorite Images">
+            <SplideSlide style="border-radius: 25px;" v-for="image in images" class="item-slider">
+                <img :src="image.name" :alt="image.title" />
+            </SplideSlide>
+            <!-- <SplideSlide class="item-slider">
+                <img src="@/assets/img/13.jpg" alt="Sample 2" />
+            </SplideSlide>-->
+        </Splide>
+    </div>
+</template>
+
+<script>
+import { reactive } from "vue"
+
+export default {
+    setup() {
+        const sliderOptions = {
+            cover: true,
+            heightRatio: 0.5,
+            type: 'loop',
+            autoplay: true
+        }
+
+        const images = reactive({
+            1: {
+                name: "src/assets/img/3.jpg",
+                title: 'Sample 1'
+            },
+            2: {
+                name: "src/assets/img/13.jpg",
+                title: 'Sample 2'
+            }
+        })
+
+        return { sliderOptions, images }
+    }
+}
+</script>
+
+<style>
+.item-slider img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,20 @@
 <template>
-    <div>
-        <p>Home</p>
+    <div class="home-container">
+        <div class="banner-slider">
+            <Slider />
+        </div>
     </div>
 </template>
 
 <script setup>
+import '@splidejs/vue-splide/css';
+import Slider from '@/components/partial/Slider.vue'
 
 </script>
 
-<style lang="scss" scoped>
+<style>
+.banner-slider {
+    width: 85%;
+    margin: auto;
+}
 </style>
